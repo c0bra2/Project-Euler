6cf736b746904fa3dbddda9bfb7ff1639a24f28c
Jacob Mason <speedyguy8@gmail.com>
2013-01-01 15:39:30 -0500

    added a couple source files and README.md

---
+++ Problem1/solution1.c
@@ -0,0 +1,20 @@
+/********************************************************************
+ *                                                                  *
+ *	If we list all the natural numbers below 10 that are multiples  *
+ *	of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples    *
+ *	is 23                                                           *
+ *                                                                  *
+ ********************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int sum = 0, i;
+
+	for (i = 1; i < 1000; i++)
+		if (!(i % 3) || !(i % 5))
+			sum += i;
+	printf("%d\n", sum);
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Project Euler Solutions
+-----------------------
+
+Here are my solutions to Project Euler, I've decided to write the solution
+in c as consicely as possible. The problem to be solved is described and included
+as a comment in the source file of the solution.
