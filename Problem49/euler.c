077cd43605f81abe5ecc90b61e913615f90429b5
Jacob Mason <speedyguy8@gmail.com>
2013-04-29 13:22:12 -0400

    Uploading solution to Problem 49

---
+++ Problem49/euler.c
@@ -0,0 +1,101 @@
+/*********************************************************
+ *                                                       *
+ * Various functions I wrote to help me solve problems   *
+ * on project euler                                      *
+ *                                                       *
+ *********************************************************/
+
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include "euler.h"
+#define MAX 10 
+
+/******************************
+ * tests if a number is prime *
+ ******************************/
+int isprime(n)
+{
+	int x;
+	int squared;
+	squared = pow(n,0.5);
+
+	if (n < 2)
+		return 0;
+	if (n == 2)
+		return 1;
+	if (n % 2 == 0)
+		return 0;
+	for (x = 3; (x < (squared + 1)); x = x + 2)
+		if (n % x == 0)
+			return 0;
+	return 1;
+}
+
+/*******************************************
+ * test to see if a number is a palindrome *                            
+ *******************************************/
+int ispalinedrome(int test)
+{
+	char reversed[MAX + 1];
+	char forwards[MAX + 1];
+
+	//convert int to string
+	sprintf(forwards, "%d", test);
+
+	//reverse forwards 
+	reverse(reversed, forwards);
+
+	//compare strings
+	if(strcmp(reversed, forwards) == 0)
+		return 1; //is palindrome 
+	else 
+		return 0;
+}
+
+/********************************
+ * reverses a string and stores *
+ * result in first argument     *
+ ********************************/
+void reverse(char *a, char * const b)
+{
+	char *c;
+
+	for (c = (b + (strlen(b) - 1)); c >= b; c--)
+	{
+		*a = *c;
+		a++; 
+	}
+	*a = '\0'; //terminate the string 
+}
+/********************
+ * triangle numbers *
+ ********************/
+int tri(int n)
+{
+	int tn;
+	tn = ((n*(n+1))/2);
+	return tn;
+}
+/*******************
+ * pentagonal nums *
+ *******************/
+int pen(int n)
+{
+	int pn;
+	pn = (n*(3*n-1)/2);
+	return pn;
+}
+
+/*****************
+ *hexagonal nums *
+ *****************/
+int hex(int n)
+{
+	int hn;
+	hn = (n*((2*n)-1));
+	return hn;
+}
+
+
+
---
+++ Problem49/euler.h
@@ -0,0 +1,11 @@
+#ifndef EULER
+#define EULER
+
+int isprime(int n);
+int ispalindrome(int test);
+void reverse(char *a, char * const b);
+int tri(int  n);
+int pen(int n);
+int hex(int n);
+
+#endif
